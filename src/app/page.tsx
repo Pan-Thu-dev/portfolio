9b9da19564eee4100c40ed48bbee026f7edc952a
Jelly_Fish <panthu252004@gmail.com>
2025-04-02 23:10:02 +0700

    feature(page) Add 'About me' page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
+import AboutSection from '@/components/About'
 
 export default function Home() {
   return (
    <main className="min-h-screen bg-white dark:bg-gray-900">
     <Navbar />
     <Hero />
+    <AboutSection />
    </main>
   );
 }
